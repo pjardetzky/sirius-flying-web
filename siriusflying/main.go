87dec3a1c90908421d160a3d03581af8d925e640
Paul Jardetzky <paul@jardetzky.com>
2020-06-04 21:14:37 -0700

    Migrate to go1.1.2 appengine runtime

--- siriusflying/main.go
+++ siriusflying/main.go
@@ -1,17 +1,41 @@
-package start
+package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 )
 
-func init() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/discovery", discovery)
-	http.HandleFunc("/training", training)
-	http.HandleFunc("/rental", rental)
-	http.HandleFunc("/about", about)
-	http.HandleFunc("/contact", contact)
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/discovery", discovery)
+	mux.HandleFunc("/training", training)
+	mux.HandleFunc("/rental", rental)
+	mux.HandleFunc("/about", about)
+	mux.HandleFunc("/contact", contact)
+
+	css := http.FileServer(http.Dir("./css/"))
+	fonts := http.FileServer(http.Dir("./fonts/"))
+	js := http.FileServer(http.Dir("./js/"))
+	img := http.FileServer(http.Dir("./img/"))
+
+	mux.Handle("/css/", http.StripPrefix("/css", css))
+	mux.Handle("/fonts/", http.StripPrefix("/fonts", fonts))
+	mux.Handle("/js/", http.StripPrefix("/js", js))
+	mux.Handle("/img/", http.StripPrefix("/img", img))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func menuMap(active string) map[string]template.HTMLAttr {
